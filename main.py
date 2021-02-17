94283bf8ba113cf1ce326d5a68fda091e5ee0831
ainesh <sannidhiainu@gmail.com>
2021-02-17 13:56:09 +0530

    Added colors to bricks

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from input import input_to, Get
 from os import system
 import numpy as np
 from time import sleep
+from colorama import Back, Fore, Style
 
 # try:
 rows = 30
@@ -11,6 +12,9 @@ columns = 80
 # t = 1/fps
 paddle_char = '='
 ball_char = 'O'
+# brick_char = '#'
+brick_length = 4
+no_of_bricks = 5
 
 
 class Paddle:
@@ -47,11 +51,15 @@ class Ball:
 
     def disp(self, arr):
         if(self.x < 2):
-            arr[self.y][2] = ball_char
+            self.x = 2
+            # self.x_vel *= -1
         elif(self.x > columns):
-            arr[self.y][columns] = ball_char
-        else:
-            arr[self.y][self.x] = ball_char
+            self.x = columns
+            # self.x_vel *= 1
+        if(self.y < 2):
+            self.y = 1
+            # self.y_vel = -1
+        arr[self.y][self.x] = ball_char
 
     def before_start_move(self, Paddle):
         self.x = Paddle.x + self.start_pos
@@ -61,7 +69,7 @@ class Ball:
 
     def move(self):
         self.y -= self.y_vel
-        self.x -= self.x_vel
+        self.x += self.x_vel
         if(self.y == 1):
             self.y_vel = -1*self.y_vel
         if(self.x <= 3 or self.x >= columns-1):
@@ -74,14 +82,59 @@ class Ball:
             self.y_vel = 1
             self.y = rows - 1
             if(Paddle.length % 2 == 0):
-                self.x_vel = ((self.x - Paddle.x) - (int)(Paddle.length/2))
+                self.x_vel = -1*((self.x - Paddle.x) - (int)(Paddle.length/2))
             else:
-                self.x_vel = (Paddle.x + (int)(Paddle.length/2) - self.x)
+                self.x_vel = -1*(Paddle.x + (int)(Paddle.length/2) - self.x)
+
+    def brick_collision(self, arr, Brick_arr):
+        if(self.y > rows or self.y < 1 or self.x < 1 or self.x > columns):
+            return
+        if((arr[self.y+1][self.x] == '0' or arr[self.y+1][self.x] == '1' or arr[self.y+1][self.x] == '2' or arr[self.y+1][self.x] == '3' or arr[self.y+1][self.x] == '4') and (self.y_vel == -1)):
+            curr = int(arr[self.y+1][self.x])
+            self.y_vel = 1
+            self.y = Brick_arr[curr].y - 1
+            if (Brick_arr[curr].strength > 0):
+                Brick_arr[curr].strength -= 1
+        if((arr[self.y-1][self.x] == '0' or arr[self.y-1][self.x] == '1' or arr[self.y-1][self.x] == '2' or arr[self.y-1][self.x] == '3' or arr[self.y-1][self.x] == '4') and (self.y_vel == 1)):
+            curr = int(arr[self.y-1][self.x])
+            self.y_vel = -1
+            self.y = Brick_arr[curr].y + brick_length
+            if (Brick_arr[curr].strength > 0):
+                Brick_arr[curr].strength -= 1
+        if((arr[self.y][self.x+1] == '0' or arr[self.y][self.x+1] == '1' or arr[self.y][self.x+1] == '2' or arr[self.y][self.x+1] == '3' or arr[self.y][self.x+1] == '4') and (self.x_vel > 0)):
+            curr = int(arr[self.y][self.x+1])
+            self.x_vel *= -1
+            self.x = Brick_arr[curr].x - 1
+            if (Brick_arr[curr].strength > 0):
+                Brick_arr[curr].strength -= 1
+        if((arr[self.y][self.x-1] == '0' or arr[self.y][self.x-1] == '1' or arr[self.y][self.x-1] == '2' or arr[self.y][self.x-1] == '3' or arr[self.y][self.x-1] == '4') and (self.x_vel < 0)):
+            curr = int(arr[self.y][self.x-1])
+            self.x_vel *= -1
+            self.x = Brick_arr[curr].x + brick_length
+            if (Brick_arr[curr].strength > 0):
+                Brick_arr[curr].strength -= 1
+
+
+class Brick:
+    def __init__(self, index):
+        self.x = np.random.randint(3, columns - 4)
+        self.y = np.random.randint(3, rows - 10)
+        self.strength = np.random.randint(1, 4)
+        self.index = index
+
+    def disp(self, arr):
+        for i in range(brick_length):
+            for j in range(brick_length):
+                arr[self.y + j][self.x + i] = self.index
+
 
 system("stty -echo")
 lives = 3
+brick_arr = [0]*no_of_bricks
+for i in range(no_of_bricks):
+    brick_arr[i] = Brick(i)
 
-for j in range(3):
+for k in range(3):
     newPaddle = Paddle(9)
     newBall = Ball(newPaddle)
     ball_move = 0
@@ -104,28 +157,50 @@ for j in range(3):
         screen[rows+1][0] = ' '
         screen[0][columns+1] = ' '
         screen[rows+1][columns+1] = ' '
-        if(not ticks%3):
+        if(not ticks % 3):
             newBall.move()
-        ticks+=1
-        ticks%=3
-        
+        ticks += 1
+        ticks %= 3
+
+        for i in range(no_of_bricks):
+            if(brick_arr[i].strength > 0):
+                brick_arr[i].disp(screen)
         newPaddle.disp(screen)
         newBall.disp(screen)
 
         if(ball_move == 1):
             newBall.paddle_collison(screen, newPaddle)
 
+        newBall.brick_collision(screen, brick_arr)
+
         if(newBall.y == rows+1):
             lives -= 1
             break
         print("LIVES: ", lives)
+        print("TIME: ", )
+        print("SCORE: ", )
 
         print('\033[0;0H')
         # print("\033[H\033[J")
 
         for i in screen:
             for j in i:
-                print(j, end="")
+                if(j == '0' or j == '1' or j == '2' or j == '3' or j == '4'):
+                    curr = int (j)
+                    if(brick_arr[curr].strength == 3):
+                        print(Fore.RED + Back.RED, end = "")
+                        print(j, end="")
+                        print(Style.RESET_ALL, end="")
+                    if(brick_arr[curr].strength == 2):
+                        print(Fore.YELLOW + Back.YELLOW, end = "")
+                        print(j, end="")
+                        print(Style.RESET_ALL, end="")
+                    if(brick_arr[curr].strength == 1):
+                        print(Fore.GREEN + Back.GREEN, end = "")
+                        print(j, end="")
+                        print(Style.RESET_ALL, end="")
+                else:
+                    print(j, end="")
 
         if(key == "d" or key == "a"):
             newPaddle.move(key)
